ea64652158ad47a48f3fb4ca038fd5e2c853a7d4
amjidislamkhan <43332701+amjidislamkhan@users.noreply.github.com>
2025-01-20 23:36:32 +0500

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,7 @@
+#pip install nltk        type this and enter in terminal first
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-#nltk.download('vader_lexicon')
+#nltk.download('vader_lexicon')  #run this once and comment it after that
 def analyze_sentiment(text):
     sia=SentimentIntensityAnalyzer()
     scores=sia.polarity_scores(text)
@@ -11,7 +12,7 @@ def analyze_sentiment(text):
     else:
         return 'neutral'
 #def main():
-text='ai is funky'
+text='ai is cool'  #change it for any sentences and check results
 print(analyze_sentiment(text))
 #if __name__ == "__main__":
    # main()
